c7d6e83c47733832a54a0150040e12852f44f01a
KarlH3644 <43153810+karl3644@users.noreply.github.com>
2024-02-01 10:44:18 +0000

    Ticket: 1234

--- src/movies/__tests__/MovieDetail.test.js
+++ src/movies/__tests__/MovieDetail.test.js
@@ -34,4 +34,5 @@ test("<MovieDetail />", async () => {
     const { getByTestId } = render(<MovieDetail match={match} />);
     await waitForElement(() => getByTestId("movie-title"));
     expect(getByTestId("movie-title").textContent).toBe(movie.title);
+    // comment
 });
